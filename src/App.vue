a91e43f1a12050ffa21d56c4757ec942ba888e89
Subramanya M Rao <subramanya11rao@gmail.com>
2022-10-20 18:26:46 +0000

    form updated

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,42 @@
 <script setup>
   import { ref } from 'vue'
+  // ref = reactive reference
   const header = ref('Shopping List App')
+const items = ref([
+  {id: 1, label: "10 party hats"},
+  {id: 2, label:"2 board games"},
+  {id: 3, label: "20 cups"}
+])
+  const newItem = ref('')
+  const newItemHighPriority = ref(false)
 </script>
 
 <template>
+<!--   Title -->
   <h1>{{ header.toLocaleUpperCase() }}</h1>
+<!--   Form -->
+  <form 
+		class="add-item-form"
+  	@submit.prevent="items.push({id: items.length + 1,label: newItem})"       
+	>
+    <input 
+    	v-model.trim="newItem"
+ 			type="text" 
+      placeholder="Add an item"
+    >
+    <label>
+      <input type="checkbox" v-model="newItemHighPriority"> 
+      High Priority
+    </label>
+    <button 
+      class="btn btn-primary"
+    >
+      Save Item
+    </button>
+   </form>
+  <ul>
+    <li v-for = "{id , label} in items" :key="id">{{ label }}</li>
+  </ul>
 </template>
 
 <style scoped>
